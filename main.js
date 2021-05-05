8ab76c38ae856e879ac9e4f67075fa58376127f3
Jun <lumjc12@gmail.com>
2021-05-05 18:54:46 +0800

    uploaded project

--- main.js
+++ main.js
@@ -1,28 +1,22 @@
 $(document).ready(function(){
   // for news section
   $.ajax({
-    url: "http://api.mediastack.com/v1/news?access_key=f6c727eec0d645dc78467fd0a06eaa71&languages=en&countries=au,us,de,kr,sg&limit=30",
+    url: "http://api.mediastack.com/v1/news?access_key=9324c3707e865e16a6dcb0a91d2dfdd2&languages=en&countries=au,us,de,kr,sg&limit=20",
     method: 'GET',
-    access_key: "f6c727eec0d645dc78467fd0a06eaa71",
+    access_key: "9324c3707e865e16a6dcb0a91d2dfdd2",
     success: function(d){
         let articles = d.data
 
         articles.forEach(function(article) {
           let singlePost = $(`
-            <div class="col-lg-6">
-                <div class="single_post post_type3 mb30">
-                  <div class="post_img">
-                      <img class= "article_img" src="${article.image}"alt="">
-                  </div>
-                  <a href="${article.url}"></a>
-                  <div class="single_post_text">
-                    <div class="meta3">	<a href="${article.url}">${article.title}</a>
-                    </div>
-                    <div class="space-10"></div>
-                    <p class="post-p">${article.description}</p>
-                  </div>
-                </div>
-              </div>
+            <div class="card">
+		<img src="${article.image}" alt="" class="card-img-top">
+		<div class="card-body">
+		  <h5 class="card-title">${article.title}</h5>
+		  <p class="card-text">${article.description}</p>
+		 <a href="${article.url}" class="btn btn-outline-success btn-sm">Read More</a>
+		</div>
+	   </div>
           `);
           if(article.image != null) {
             return $("#latest > div").append(singlePost);
@@ -38,37 +32,27 @@ $(document).ready(function(){
   searchBtn.click(function(e){
     let searchText = inputBar.val()
     $.ajax({
-      url: `http://api.mediastack.com/v1/news?access_key=f6c727eec0d645dc78467fd0a06eaa71&keywords=${searchText}&limit=10`,
+      url: `http://api.mediastack.com/v1/news?access_key=9324c3707e865e16a6dcb0a91d2dfdd2&keywords=${searchText}&limit=100`,
       method: 'GET',
-      access_key: "f6c727eec0d645dc78467fd0a06eaa71",
+      access_key: "9324c3707e865e16a6dcb0a91d2dfdd2",
       success: function(f){
         $("#latest > div").empty();
-        // searchText.
         let search = f.data
         search.forEach(function(searches){
-          // console.log(searches)
           let singlePost = $(`
-            <div class="col-lg-6">
-                <div class="single_post post_type3 mb30">
-                  <div class="post_img">
-                      <img class= "article_img" src="${searches.image}"alt="">
-                  </div>
-                  <a href="${searches.url}"></a>
-                  <div class="single_post_text">
-                    <div class="meta3">	<a href="${searches.url}">${searches.title}</a>
-                    </div>
-                    <div class="space-10"></div>
-                    <p class="post-p">${searches.description}</p>
-                  </div>
-                </div>
-              </div>
-          `);
+          <div class="card">
+          <img src="${searches.image}" alt="" class="card-img-top">
+          <div class="card-body">
+          <h5 class="card-title">${searches.title}</h5>
+          <p class="card-text">${searches.description}</p>
+          <a href="${searches.url}" class="btn btn-outline-success btn-sm">Read More</a>
+          </div>
+          </div>
+           `);
           if(searches.image != null) {
-            return $("#latest > div").append(singlePost);
+            $("#latest > div").append(singlePost);
           }
-        })
-          
-          
+        }) 
       }
     })
   })
@@ -77,29 +61,28 @@ $(document).ready(function(){
 
 // Popular Article
 $.ajax({
-  url: "http://api.mediastack.com/v1/news?access_key=f6c727eec0d645dc78467fd0a06eaa71&languages=en&sort=popularity&limit=10&source=CNN Europe",
+  url: "http://api.mediastack.com/v1/news?access_key=9324c3707e865e16a6dcb0a91d2dfdd2&languages=en&sort=popularity&limit=10&source=CNN Europe",
   method: 'GET',
-  access_key: "f6c727eec0d645dc78467fd0a06eaa71",
+  access_key: "9324c3707e865e16a6dcb0a91d2dfdd2",
   success: function(p){
-    console.log(p)
     let popWidgets = p.data
     popWidgets.forEach(function(popWidget){
       if(popWidget.image != null){
-        let widgetPost = $(`<div class="post_img">
-        <div class="img_wrap">
-          <a href="#">
+        let widgetPost = $(`<div class="single_post widgets_small">
+        <div class="post_img">
+          <div class="img_wrap">
+            <a href="${popWidget.url}">
             <img class="widget_image" src="${popWidget.image}" alt="">
-          </a>
-        </div>
-      </div>
-      <div class="single_post_text">
-        <div class="meta2 meta_separator1">	<a href="#">${popWidget.title}</a>
-        </div>
-        <h4><a href="${popWidget.url}">${popWidget.description}</a></h4>
+            </a>
+          </div>
+          </div>
+          <div class="single_post_text">
+          <div class="meta2 meta_separator1">${popWidget.title}<a href="${popWidget.url}"></a>
+          </div>
+          <h4><a href="${popWidget.url}"></a></h4>
+          </div>
       </div>`)
-      console.log(popWidget.title)
-      console.log(popWidget.image)
-      return $(".tab-content-widget > div").append(widgetPost);
+       $(".tab-content-widget > div").append(widgetPost);
     }
      })
     }
@@ -108,15 +91,24 @@ $.ajax({
 
 // Carousel Page
 $.ajax({
-  url: "http://api.mediastack.com/v1/news?access_key=f6c727eec0d645dc78467fd0a06eaa71&languages=en&sort=popularity&limit=30&source=CNN Europe",
+  url: "http://api.mediastack.com/v1/news?access_key=9324c3707e865e16a6dcb0a91d2dfdd2&languages=en&countries=au,us,de,kr,sg&limit=20",
   method: 'GET',
-  access_key: "f6c727eec0d645dc78467fd0a06eaa71",
-  success: function(c){
+  access_key: "9324c3707e865e16a6dcb0a91d2dfdd2",
+  success: function(c){ 
+    $("#carouselExampleControls").carousel('dispose');
+    console.log(c)
     let carousels = c.data
-    // console.log(c)
     carousels.forEach(function(carousel){
-    $('.carousel_img').attr('src',`${carousel.image}`)
+      let carou = $(`<div class="carousel-item">
+			<div class="d-flex justify-content-center">
+			<img class=carou_img class="d-block w-100" src="${carousel.image}" alt="Second slide">
+		  </div>
+		  </div>`)
+      if(carousel.image != null) {
+        $('.carousel-inner').append(carou)
+      }
     })
+    $("#carouselExampleControls").carousel(0);
   }
 })
 
@@ -127,12 +119,24 @@ $.ajax({
     const random = Math.floor(Math.random() * random_cat.length);
 
     $.ajax({
-      url: `http://api.mediastack.com/v1/news?access_key=f6c727eec0d645dc78467fd0a06eaa71&categories=${random_cat[random]}&limit=1&languages=en`,
+      url: `http://api.mediastack.com/v1/news?access_key=9324c3707e865e16a6dcb0a91d2dfdd2&categories=${random_cat[random]}&limit=1&languages=en`,
       method: 'GET',
-      access_key: "f6c727eec0d645dc78467fd0a06eaa71",
+      access_key: "9324c3707e865e16a6dcb0a91d2dfdd2",
       success: function(f){
-        console.log(f.data[0])
-        window.location.replace(f.data[0].url)
+        window.location.href = f.data[0].url
       }
     })
   })
+
+ // Activate Carousel
+$("#myCarousel").carousel();
+
+// Enable Carousel Indicators
+$(".item").click(function(){
+  $("#myCarousel").carousel(1);
+});
+
+// Enable Carousel Controls
+$(".left").click(function(){
+  $("#myCarousel").carousel("prev");
+});
